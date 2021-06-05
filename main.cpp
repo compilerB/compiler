565dcee94118b02b75aefdba8bfbd7113361113e
coffee0920 <43335128+coffee0920@users.noreply.github.com>
2021-06-05 11:32:28 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,9 +7,8 @@
 #define strMember(ch, set) (strchr(set, ch) != NULL)
 FILE *fp_in, *fp_out;
 
-/*** 0601版 錯誤則跳出  ***/
+/*** 0605版 移除不必要註解  ***/
 
-// char UNKNOWN[] = "else case";
 char End[] = {13,10};
 char Int[] = "|0|1|2|3|4|5|6|7|8|9|";
 char Var[] = "|A|a|B|b|C|c|D|d|E|e|F|f|G|g|H|h|I|i|J|j|K|k|L|l|M|m|N|n|O|o|P|p|Q|q|R|r|S|s|T|t|U|u|V|v|W|w|X|x|Y|y|Z|z|";
@@ -141,7 +140,7 @@ Node *insert_N(Node *current, char ch[], Token token)  //加入字元
 }
 
 
-void print(node *current)   //將所有linked list 列印出來，用於檢查
+void print(node *current)
 {
     while(current != NULL)
     {
@@ -150,7 +149,7 @@ void print(node *current)   //將所有linked list 列印出來，用於檢查
     }
 }
 
-void print_N(Node *current)   //將所有linked list 列印出來，用於檢查
+void print_N(Node *current)
 {
     while(current != NULL)
     {
@@ -220,69 +219,59 @@ Token getToken(char c)//取得Token
 
 void lexer(node *current,Node *head_N)
 {
-    Token PreToken=NUL;   //前一個字元的Token
-    Token CurToken=NUL;   //當前字元的Token
-    Node *temp;           //暫存現在加入的點
-    char tempstr[20]="";  //暫存字串的陣列
-    char zerostr[20]="";  //清空暫存字串用的陣列
-    char cur_char[1];     //暫存當前字元的陣列
+    Token PreToken=NUL;
+    Token CurToken=NUL;
+    Node *temp;
+    char tempstr[20]="";
+    char zerostr[20]="";
+    char cur_char[1];
 
-    while (current != NULL)  //儲存讀入字元的Linked List節點不為空
+    while (current != NULL)
     {
-        cur_char[0]=current->ch;          //從儲存讀入字元的Linked List讀字元出來
-        CurToken=getToken(cur_char[0]);   //取得當前字元的Token
+        cur_char[0]=current->ch;
+        CurToken=getToken(cur_char[0]);
 
-        if(PreToken==NUL)        //前一個Token是空的代表是第一個
+        if(PreToken==NUL)
         {
-            //第一個不能是(數字、+-*/、=)
             if(CurToken == 2)
             {
                 printf("Can't start with a number\n");
                 printf("%s\n",cur_char);
                 exit(0);
-//                printf("EXIT\n");
             }
             else if(CurToken == 4||CurToken==5)
             {
                 printf("Can't start with an operator\n");
                 printf("%s\n",cur_char);
                 exit(0);
-//                printf("EXIT\n");
             }
             else if(CurToken == 6)
             {
                 printf("Can't start with an assign\n");
                 printf("%s\n",cur_char);
                 exit(0);
-//                printf("EXIT\n");
             }
-            strcat(tempstr,cur_char);   //將這個字元先放進Temp裡面
-            PreToken = CurToken;        //當前字元Token存入前一個Token
-            CurToken = NUL;             //當前字元Token清空
+            strcat(tempstr,cur_char);
+            PreToken = CurToken;
+            CurToken = NUL;
         }
         else //P.T.!=NULL
         {
             if(PreToken == CurToken)
             {
-                if(CurToken == 4||CurToken==5)  //eg. ++ -- +- -+ ** // */ /*
+                if(CurToken == 4||CurToken==5)
                 {
                     printf("Two Consecutive Operator Error\n");
                     printf("%s %s\n",tempstr,cur_char);
-//                    temp = insert_N(head_N,tempstr,PreToken);
-//                    parsing(temp);
-//                    strcpy(tempstr,zerostr);
-//                    strcat(tempstr,cur_char);
                     exit(0);
-//                    printf("EXIT\n");
                 }
-                else if(CurToken==6)   // eg. ==
+                else if(CurToken==6)
                 {
                     printf("Two Assign In a Row\n");
                     printf("%s %s\n",tempstr,cur_char);
                     exit(0);
-//                    printf("EXIT\n");
                 }
-                else if(CurToken==7||CurToken==8)  //左括、右括分開儲存
+                else if(CurToken==7||CurToken==8)
                 {
                     temp = insert_N(head_N,tempstr,PreToken);
                     parsing(temp);
@@ -294,12 +283,12 @@ void lexer(node *current,Node *head_N)
                     strcat(tempstr,cur_char);
                 }
             }
-            else if(PreToken == 3 && CurToken == 2)  //變數以英文開頭，後面可以接數字
+            else if(PreToken == 3 && CurToken == 2)
             {
                 CurToken = PreToken;
                 strcat(tempstr,cur_char);
             }
-            else  //P.T.!=C.T.
+            else
             {
                 temp = insert_N(head_N,tempstr,PreToken);
                 parsing(temp);
@@ -311,24 +300,21 @@ void lexer(node *current,Node *head_N)
         }
         current = current->next;
     }
-    //需要多跑一次，處理最後一個字元
     if(PreToken == CurToken)
     {
-        if(CurToken == 4||CurToken==5)  //eg. ++ -- +- -+ ** // */ /*
+        if(CurToken == 4||CurToken==5)
         {
             printf("Two Consecutive Operator Error\n");
             printf("%s %s\n",tempstr,cur_char);
             exit(0);
-//            printf("EXIT\n");
         }
         else if(CurToken==6)
         {
-            printf("Two Assign In a Row\n"); //eg. ==
+            printf("Two Assign In a Row\n");
             printf("%s %s\n",tempstr,cur_char);
             exit(0);
-//            printf("EXIT\n");
         }
-        else if(CurToken==7||CurToken==8) //左括、右括分開儲存
+        else if(CurToken==7||CurToken==8)
         {
             temp = insert_N(head_N,tempstr,PreToken);
             parsing(temp);
@@ -340,12 +326,12 @@ void lexer(node *current,Node *head_N)
             strcat(tempstr,cur_char);
         }
     }
-    else if(PreToken == 3 && CurToken == 2) //變數以英文開頭，後面可以接數字
+    else if(PreToken == 3 && CurToken == 2)
     {
         CurToken = PreToken;
         strcat(tempstr,cur_char);
     }
-    else  //P.T.!=C.T.
+    else
     {
         temp = insert_N(head_N,tempstr,PreToken);
         parsing(temp);
@@ -358,47 +344,42 @@ void lexer(node *current,Node *head_N)
 
 void parsing(Node *current)
 {
-    bool assigflag=false;   //assign flag，每一行只能有一個等於
+    bool assigflag=false;
     Node *temp;
-    temp=current->prev;     //看前一個字串的Token
-    if(temp->token==10)     //遇到空白再往前看
+    temp=current->prev;
+    if(temp->token==10)
     {
         temp=temp->prev;
     }
-    switch(current->token)  //根據當前存入之字串Token，判斷是否合法
+    switch(current->token)
     {
     case 0: //UNKNOWN
 
         printf("Unknown Input Error\n");
         printf("%s\n",current->data);
         exit(0);
-//        printf("EXIT\n");
         break;
 
     case 1: //END
 
-        if (temp->token ==4||temp->token ==5) // eg. *\n
+        if (temp->token ==4||temp->token ==5)
         {
 
             printf("Operator End Error\n");
             printf("%s\n",temp->data);
             exit(0);
-//            printf("EXIT\n");
         }
-        else if (temp->token ==6 ) // eg. =\n
+        else if (temp->token ==6 )
         {
 
             printf("Assign End Error\n");
             printf("%s\n",temp->data);
             exit(0);
-//            printf("EXIT\n");
-
         }
-        if(parencount!=0)  //括號不成對
+        if(parencount!=0)
         {
             printf("Unpaired Parentheses Error\n");
             exit(0);
-//            printf("EXIT\n");
         }
         assigflag=false;
         parencount = 0;
@@ -406,151 +387,133 @@ void parsing(Node *current)
 
     case 2: //INT
 
-        if(temp->token==2||temp->token ==3)  //eg. a1 4 || 15 3
+        if(temp->token==2||temp->token ==3)
         {
             printf("Two Consecutive Term Error\n");
             printf("%s %s\n",temp->data,current->data);
             exit(0);
-//            printf("EXIT\n");
         }
-        else if(temp->token==8)  //eg. (15)2
+        else if(temp->token==8)
         {
             printf("Hidden Operator Error\n");
             printf("%s %s\n",temp->data,current->data);
             exit(0);
-//            printf("EXIT\n");
         }
-        else if(temp->token==1)  //每行開頭不能是數字
+        else if(temp->token==1)
         {
             printf("Can't start with a number\n");
             printf("%s\n",current->data);
-                exit(0);
-//            printf("EXIT\n");
+            exit(0);
         }
         break;
 
     case 3: //VAR
 
-        if(temp->token==2||temp->token ==3) //eg. 4 a1  || a1 a3
+        if(temp->token==2||temp->token ==3)
         {
             printf("Two Consecutive Term Error\n");
             printf("%s %s\n",temp->data,current->data);
             exit(0);
-//            printf("EXIT\n");
         }
-        else if(temp->token==8) //eg. (15)a2
+        else if(temp->token==8)
         {
             printf("Hidden Operator Error\n");
             printf("%s %s\n",temp->data,current->data);
             exit(0);
-//            printf("EXIT\n");
         }
         break;
 
     case 4: //ADDSUB
 
-        if(temp->token== 7)  //eg. (+
+        if(temp->token== 7)
         {
             printf("Left Parentheses Operator Error\n");
             printf("%s %s\n",temp->data,current->data);
             exit(0);
-//            printf("EXIT\n");
         }
-        else if(temp->token == 5) //eg. *+
+        else if(temp->token == 5)
         {
             printf("Two Consecutive Operator Error\n");
             printf("%s %s\n",temp->data,current->data);
             exit(0);
-//            printf("EXIT\n");
         }
-        else if(temp->token==1)   //每行開頭不能是 +-
+        else if(temp->token==1)
         {
             printf("Can't start with an operator\n");
             printf("%s\n",current->data);
             exit(0);
-//            printf("EXIT\n");
         }
-        else if(temp->token == 6) //eg. =+
+        else if(temp->token == 6)
         {
             printf("Assign Operator Error\n");
             printf("%s %s\n",temp->data,current->data);
             exit(0);
-//            printf("EXIT\n");
         }
         break;
 
     case 5: //MULDIV
 
-        if(temp->token==7)       //eg. (*
+        if(temp->token==7)
         {
             printf("Left Parentheses Operator Error\n");
             printf("%s %s\n",temp->data,current->data);
             exit(0);
-//            printf("EXIT\n");
         }
-        else if(temp->token==4)  //eg. -/
+        else if(temp->token==4)
         {
             printf("Two Consecutive Operator Error\n");
             printf("%s %s\n",temp->data,current->data);
             exit(0);
-//            printf("EXIT\n");
         }
-        else if(temp->token==1)  //每行開頭不能是 */
+        else if(temp->token==1)
         {
             printf("Can't start with an operator\n");
             printf("%s\n",current->data);
-                exit(0);
-//            printf("EXIT\n");
+            exit(0);
         }
-        else if(temp->token == 6) //eg. =*
+        else if(temp->token == 6)
         {
             printf("Assign Operator Error\n");
             printf("%s %s\n",temp->data,current->data);
             exit(0);
-//            printf("EXIT\n");
         }
         break;
 
     case 6: //ASSIGN
 
-        if(temp->token==7)  //eg. (=
+        if(temp->token==7)
         {
             printf("Left Parentheses Assign Error\n");
             printf("%s %s\n",temp->data,current->data);
             exit(0);
-//            printf("EXIT\n");
         }
-        else if(temp->token==4||temp->token ==5)  //eg. -=
+        else if(temp->token==4||temp->token ==5)
         {
             printf("Operator Assign Error\n");
             printf("%s %s\n",temp->data,current->data);
             exit(0);
-//            printf("EXIT\n");
         }
-        else if(temp->token==1) //每行開頭不能是 =
+        else if(temp->token==1)
         {
             printf("Can't start with an assign\n");
             printf("%s\n",current->data);
             exit(0);
-//            printf("EXIT\n");
         }
-        if(assigflag)  //Only one assign in a row
+        if(assigflag)
         {
             printf("Two Assigns In a Row\n");
             exit(0);
-//            printf("EXIT\n");
         }
         assigflag=true;
         break;
 
     case 7: //LPAREN
 
-        if(temp->token==2||temp->token ==3)       // eg. 3(6) || a2(5)
+        if(temp->token==2||temp->token ==3)
         {
             printf("Hidden Operator Error\n");
             printf("%s %s\n",temp->data,current->data);
             exit(0);
-//            printf("EXIT\n");
         }
         parencount++;
         break;
@@ -558,40 +521,34 @@ void parsing(Node *current)
     case 8: //RPAREN
 
         parencount--;
-        if(parencount<0) //判斷括號不成對
+        if(parencount<0)
         {
             printf("Extra Right Parentheses\n");
             exit(0);
-//            printf("EXIT\n");
         }
-        if(temp->token==4||temp->token ==5)  //eg. +)
+        if(temp->token==4||temp->token ==5)
         {
             printf("Operator Right Parentheses Error\n");
             printf("%s %s\n",temp->data,current->data);
             exit(0);
-//            printf("EXIT\n");
         }
-        else if(temp->token==6)  //eg. =)
+        else if(temp->token==6)
         {
             printf("Assign Right Parentheses Error\n");
             printf("%s %s\n",temp->data,current->data);
             exit(0);
-//            printf("EXIT\n");
         }
-        else if(temp->token==7)  //eg. ()
+        else if(temp->token==7)
         {
             printf("Nothing in Parentheses Error\n");
             printf("%s %s\n",temp->data,current->data);
             exit(0);
-//            printf("EXIT\n");
         }
         break;
 
     case 9: //NUL
-//        printf("Case NUL\n");
         break;
     case 10: //SPACE
-//            printf("Case SPACE\n");
         break;
     }
 
@@ -620,15 +577,11 @@ int main(int argc, char *argv[])
 
     while((ch = getc(fp_in)) != EOF)
     {
-//      if(ch != '\n')      //決定是否要存換行
         insert(head,ch);
     }
     insert(head,'\n');
-
-    // print(head->next);
     lexer(head->next,head_N);
-
-    //delete_SPACE(head_N->next);
+	
     printf("\n**********\n");
     print_N(head_N->next);
     
